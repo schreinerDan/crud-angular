126c43c9df0658ebbd3ff3be0b54244abb22f3ba
Daniel Schreiner <schreiner.daniel@gmail.com>
2021-11-12 20:25:40 -0300

    Teste

--- src/app/users/users.module.ts
+++ src/app/users/users.module.ts
@@ -1,21 +1,23 @@
-
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-
+import { MatButtonToggleModule } from '@angular/material/button-toggle';
 import { MatCardModule } from '@angular/material/card';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatIconModule } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input';
 import { MatTableModule } from '@angular/material/table';
+import {MatBadgeModule} from '@angular/material/badge';
 import { RouterModule } from '@angular/router';
-import { AddComponent } from './add/add.component';
 
+import { AddComponent } from './add/add.component';
 import { DeleteComponent } from './delete/delete.component';
 import { EditComponent } from './edit/edit.component';
 import { UsersRoutingModule } from './users-routing.module';
 import { UsersComponent } from './users/users.component';
 import { ViewComponent } from './view/view.component';
+import { MatButtonModule } from '@angular/material/button';
+
 
 
 
@@ -40,7 +42,10 @@ import { ViewComponent } from './view/view.component';
     RouterModule,
     MatInputModule,
     MatFormFieldModule,
+    MatButtonToggleModule,
     MatIconModule,
+    MatBadgeModule,
+    MatButtonModule
 
   ],
   providers:[
--- src/app/users/users/users.component.ts
+++ src/app/users/users/users.component.ts
@@ -1,7 +1,9 @@
+
 import { Observable } from 'rxjs';
 import { UsersService } from './../services/users.service';
 import { Component, OnInit } from '@angular/core';
 import { User } from '../models/user';
+import { DIR_DOCUMENT_FACTORY } from '@angular/cdk/bidi/dir-document-token';
 
 
 @Component({
@@ -10,6 +12,9 @@ import { User } from '../models/user';
   styleUrls: ['./users.component.scss']
 })
 export class UsersComponent implements OnInit {
+
+  hidden = false;
+
 
    public users: User[]=[];
    public displayedColumns = ['id','username','password','is_enabled','register_date','name','surname','email','phone','actions'];
@@ -31,6 +36,36 @@ export class UsersComponent implements OnInit {
       }
     );
   }
+  public toggleDuplicatedFilter() {
+    this.hidden = !this.hidden;
+
+    if(this.hidden==true){
+      this.filterByDuplicate();
+    }
+    else{
+      this.getAllUsers();
+    }
+
+  }
+
+  public cleanList(){
+    this.users = [];
+  }
+  public filterByDuplicate(){
+    const usersDuplicated: User[] = [];
+    this.users.forEach((item) => {
+      if (this.users.filter(i => i.name === item.name).length > 1 &&
+          this.users.filter(i => i.username === item.username).length > 1) {
+
+            usersDuplicated.push(item);
+      }
+
+    });
+
+    this.users = usersDuplicated;
+
+  }
+
   public searchUsers(key: string): void{
     console.log(key);
     const result: User[] =[];
