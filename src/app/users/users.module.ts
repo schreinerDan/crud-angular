cb9a2480ff49b9d8460eac9bbf23095b9f4d2426
Daniel Schreiner <schreiner.daniel@gmail.com>
2021-11-12 21:16:55 -0300

    Teste Filters

--- src/app/users/users.module.ts
+++ src/app/users/users.module.ts
@@ -2,7 +2,8 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-
+import { MatButtonModule } from '@angular/material/button';
+import { MatButtonToggleModule } from '@angular/material/button-toggle';
 import { MatCardModule } from '@angular/material/card';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatIconModule } from '@angular/material/icon';
@@ -41,6 +42,8 @@ import { ViewComponent } from './view/view.component';
     MatInputModule,
     MatFormFieldModule,
     MatIconModule,
+    MatButtonToggleModule,
+    MatButtonModule
 
   ],
   providers:[
--- src/app/users/users/users.component.ts
+++ src/app/users/users/users.component.ts
@@ -1,7 +1,7 @@
-import { Observable } from 'rxjs';
-import { UsersService } from './../services/users.service';
 import { Component, OnInit } from '@angular/core';
+
 import { User } from '../models/user';
+import { UsersService } from './../services/users.service';
 
 
 @Component({
@@ -10,7 +10,8 @@ import { User } from '../models/user';
   styleUrls: ['./users.component.scss']
 })
 export class UsersComponent implements OnInit {
-
+   public hidden = false;
+   public hasDuplicated = false;
    public users: User[]=[];
    public displayedColumns = ['id','username','password','is_enabled','register_date','name','surname','email','phone','actions'];
 
@@ -24,13 +25,50 @@ export class UsersComponent implements OnInit {
     this.getAllUsers();
   }
 
+  public toggleDuplicatedFilter() {
+
+    this.hidden = !this.hidden;
+
+    if(this.hidden==true){
+      this.filterByDuplicate();
+    }else{
+      this.getAllUsers();
+    }
+
+  }
+
+  public clearList(){
+    this.users = [];
+    this.hasDuplicated=false;
+  }
+
+  public filterByDuplicate(){
+    const usersDuplicated: User[] = [];
+    this.users.forEach((item) => {
+      if (this.users.filter(i => i.name === item.name).length > 1 &&
+          this.users.filter(i => i.username === item.username).length > 1) {
+            usersDuplicated.push(item);
+      }
+
+    });
+
+    this.hasDuplicated = usersDuplicated.length==0 ? false : true;
+
+    this.users = this.hasDuplicated ? usersDuplicated : this.users;
+
+  }
+
+
   public getAllUsers():void{
+    this.hasDuplicated=false;
     this.userServices.find().subscribe(
       (response: User[])=>{
         this.users = response;
       }
     );
   }
+
+
   public searchUsers(key: string): void{
     console.log(key);
     const result: User[] =[];
@@ -39,12 +77,14 @@ export class UsersComponent implements OnInit {
          user.surname.toLowerCase().indexOf(key.toLowerCase())!== -1 ||
          user.username.toLowerCase().indexOf(key.toLowerCase())!== -1 ||
          user.email.toLowerCase().indexOf(key.toLowerCase())!== -1) {
+
         result.push(user);
 
       }
     }
 
     this.users = result;
+
     if (result.length ===0 || !key)
     {
         this.getAllUsers();
